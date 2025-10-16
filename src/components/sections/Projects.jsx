305b5fcf88c514b92c47b5c0e54e33e7e85bf7c7
Aaron-GF <aarongarcia567@gmail.com>
2025-10-16 16:53:31 +0200

    feat: create projects.js to add proyects and show on project section, add project section styles and responsive. feat: rename any projects images

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,7 +1,83 @@
+import { projects } from "@/lib/projects.js";
+
 export default function Projects() {
   return (
     <section id="Projects">
       <h2>Proyectos</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 px-6">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="relative group rounded-2xl overflow-hidden bg-background border-2 border-primary drop-shadow-secondary drop-shadow-md min-h-70 cursor-pointer"
+          >
+            <div className="h-full w-full">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110 group-hover:opacity-50"
+              />
+            </div>
+
+            {/* Overlay */}
+            <div className="absolute inset-0 flex flex-col justify-evenly items-center text-center opacity-0 group-hover:opacity-100 transition-opacity duration-500 bg-gradient-to-b from-transparent to-secondary/80 px-4">
+              <h3 className="text-2xl font-semibold">{project.title}</h3>
+              <p className="text-sm mb-2 text-white">{project.description}</p>
+
+              {/* Enlaces */}
+              <div className="flex gap-6">
+                <a
+                  href={project.links.demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex justify-center items-center w-10 h-10 rounded-full bg-white border border-secondary outline-2 outline-white text-gray-900 hover:scale-110 transition-transform"
+                >
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    className="lucide lucide-external-link-icon lucide-external-link size-6"
+                  >
+                    <path d="M15 3h6v6" />
+                    <path d="M10 14 21 3" />
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+                  </svg>
+                </a>
+                <a
+                  href={project.links.code}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex justify-center items-center w-10 h-10 rounded-full bg-white border border-secondary outline-2 outline-white text-gray-900 hover:scale-110 transition-transform"
+                >
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    className="lucide lucide-code-xml-icon lucide-code-xml size-6"
+                  >
+                    <path d="m18 16 4-4-4-4" />
+                    <path d="m6 8-4 4 4 4" />
+                    <path d="m14.5 4-5 16" />
+                  </svg>
+                </a>
+              </div>
+
+              {/* tecnologias y herramientas usadas en el proyecto */}
+              <div className="flex flex-wrap justify-center gap-2">
+                {project.tools.map((tool, i) => (
+                  <span
+                    key={i}
+                    className="flex items-center justify-center bg-background text-primary font-semibold text-xs rounded-lg min-h-5 min-w-24"
+                  >
+                    {tool}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/lib/projects.js
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    title: "Juego de fútbol - Footdle",
+    image: "/images/projects/footdle-football-app/screen-capture.png",
+    description:
+      "Juego web inspirado en tic-tac-toe para aficionados al fútbol. Permite seleccionar casillas del tablero y descubrir jugadores que han jugado en ambos equipos relacionados con esa posición.",
+    links: {
+      demo: "https://footdle-aa.vercel.app/",
+      code: "https://github.com/Aaron-GF/footdle",
+    },
+    tools: ["Next.js", "React", "JavaScript", "Jest", "Tailwind CSS"],
+  },
+  {
+    title: "Weather App",
+    image: "/images/projects/weather-app/screen-capture.png",
+    description:
+      "Aplicación del tiempo que muestra datos actuales en tiempo real, mostrando diversos parámetros. Permite buscar por lugar mostrando coincidencias y tiene un diseño responsivo.",
+    links: {
+      demo: "https://weather-app-twister.vercel.app/",
+      code: "https://github.com/Aaron-GF/weather-app.git",
+    },
+    tools: [
+      "Next.js",
+      "React",
+      "JavaScript",
+      "Open Weather API",
+      "Tailwind CSS",
+    ],
+  },
+  {
+    title: "App música Jammming",
+    image: "/images/projects/jammming-music-player/player.png",
+    description:
+      "Jammming es una aplicación web que permite a los usuarios buscar canciones, reproducirlas, organizarlas en listas personalizadas y guardar estas playlists directamente en su cuenta de Spotify.",
+    links: {
+      demo: "https://reproductor-musica-jammming.vercel.app/",
+      code: "https://github.com/Aaron-GF/reproductor-musica-jammming",
+    },
+    tools: ["Vite + React", "CSS modules", "JavaScript", "Spotify Web API"],
+  },
+];
