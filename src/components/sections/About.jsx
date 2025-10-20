4ac2c44c672e5f012a032b7e98f8875f090fdeb8
Aaron-GF <aarongarcia567@gmail.com>
2025-10-20 20:33:15 +0200

    refactor: change about description

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,22 +3,22 @@ export default function About() {
     <section id="About" className="flex flex-col gap-5">
       <h2>Sobre mí</h2>
       <p>
-        Mi nombre es Aarón, soy una persona curiosa y autodidacta, he decidido
-        dar un giro en mi camino laboral hacia el desarrollo web ya que sentía
-        que lo que hacía antes no me motivaba y necesitaba encontrar algo que
-        realmente si lo hiciera. Lo que he ido aprendiendo es a base de cursos,
-        práctica, prueba y error. Voy a dejar un enlace a mi perfil de linkedin
-        en el que iré subiendo los cursos y certificados que vaya consiguiendo.
+        Mi nombre es Aarón, desde hace tiempo me atrae el mundo de la
+        programación y la tecnología. Aunque mi camino profesional inicial tomó
+        otra dirección, siempre tuve presente mi interés como desarrollador. Hoy
+        estoy enfocado en aprender y mejorar mis habilidades.
       </p>
       <p>
-        El propósito de esta web es que sea el reflejo de lo que voy aprendiendo
-        y una forma de poder mostrar mis proyectos. Lo actualizaré a medida que
-        avanzo, sobre todo en la sección de proyectos, donde iré subiendo lo que
-        vaya creando.
+        Práctico constantemente con cursos y proyectos personales, de momento
+        enfocado en front-end y aprendizaje continuo de nuevas tecnologías. En
+        esta web comparto los proyectos en los que trabajo, como reflejo de mi
+        proceso de aprendizaje y crecimiento.
       </p>
       <p>
-        Este portfolio se ha desarrollado con Vite, React y TailwindCss. El
-        diseño es responsivo y se adapta a diferentes pantallas.
+        El sitio ha sido creado con Vite, React y TailwindCSS, siguiendo un
+        diseño moderno y responsivo que se adapta a diferentes dispositivos.
+        Cada detalle está pensado para mejorar funcionalidad, estética y
+        rendimiento.
       </p>
     </section>
   );
